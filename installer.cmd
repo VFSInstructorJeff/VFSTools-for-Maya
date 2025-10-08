483a458bd4561f7bf4ffbcb32f618ea95930dc1d
Jose Munguia <jose123ma@hotmail.com>
2025-10-08 13:06:28 -0400

    Installer available for 2026 version

--- installer.cmd
+++ installer.cmd
@@ -4,5 +4,6 @@ xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\scripts\baking_tool
 xcopy /s/y "%cd%\uv_tools" "%userprofile%\Documents\maya\scripts\uv_tools\"
 xcopy /s/y "%cd%\shelf_Jose_Ivy_Tools.mel" "%userprofile%\Documents\maya\2024\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_Jose_Ivy_Tools.mel" "%userprofile%\Documents\maya\2025\prefs\shelves\"
+xcopy /s/y "%cd%\shelf_Jose_Ivy_Tools.mel" "%userprofile%\Documents\maya\2026\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_Jose_Ivy_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
 xcopy /s/y "%cd%\userSetup.py" "%userprofile%\Documents\maya\scripts\"
