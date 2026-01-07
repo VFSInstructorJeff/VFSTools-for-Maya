cf9451100eff64ef0db4fc84baf3317c385bd075
Laura Renis <devwmiyo@gmail.com>
2026-01-07 15:41:00 -0800

    Added icons folder to installer
    - Icons folder will also be copied to VFSTools folder within the Maya directory
    - Placeholder LD custom icon added to icons folder

--- installer.cmd
+++ installer.cmd
@@ -9,6 +9,7 @@ xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\VFSTools\baking_too
 xcopy /s/y "%cd%\uv_tools" "%userprofile%\Documents\maya\VFSTools\uv_tools\"
 xcopy /s/y "%cd%\animation_tools" "%userprofile%\Documents\maya\VFSTools\animation_tools\"
 xcopy /s/y "%cd%\leveldesign_tools" "%userprofile%\Documents\maya\VFSTools\leveldesign_tools\"
+xcopy /s/y "%cd%\icons" "%userprofile%\Documents\maya\VFSTools\icons\"
 
 xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2024\prefs\shelves\"
