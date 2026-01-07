8a6146a71ba3532826efe3f5d807eadad4d8c2e6
Laura Renis <devwmiyo@gmail.com>
2026-01-07 13:51:18 -0800

    Changed folder structure initialization
    - Changed installer so it would initialize the VFSTools folders on the new location
    - Added Maya.env file so the new location would work
    - Changed the shelf file to include buttons for Roman's tool and the LD tool

--- installer.cmd
+++ installer.cmd
@@ -1,9 +1,17 @@
 @echo off
-xcopy /s/y "%cd%\layer_editor_tools" "%userprofile%\Documents\maya\scripts\layer_editor_tools\"
-xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\scripts\baking_tools\"
-xcopy /s/y "%cd%\uv_tools" "%userprofile%\Documents\maya\scripts\uv_tools\"
+xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2023"
+xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2024"
+xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2025"
+xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2026"
+
+xcopy /s/y "%cd%\layer_editor_tools" "%userprofile%\Documents\maya\VFSTools\layer_editor_tools\"
+xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\VFSTools\baking_tools\"
+xcopy /s/y "%cd%\uv_tools" "%userprofile%\Documents\maya\VFSTools\uv_tools\"
+xcopy /s/y "%cd%\animation_tools" "%userprofile%\Documents\maya\VFSTools\animation_tools\"
+xcopy /s/y "%cd%\leveldesign_tools" "%userprofile%\Documents\maya\VFSTools\leveldesign_tools\"
+
+xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2024\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2025\prefs\shelves\"
 xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2026\prefs\shelves\"
-xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
-xcopy /s/y "%cd%\userSetup.py" "%userprofile%\Documents\maya\scripts\"
+
