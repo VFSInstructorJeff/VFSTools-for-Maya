43644e267a3379febd27e12f8dea633fad292f10
Laura Renis <devwmiyo@gmail.com>
2026-01-12 15:26:43 -0800

    Folder structure changes
    Put the Maya.env file and the self_VFS_Tools.mel file in the VFSTools folder

--- installer.cmd
+++ installer.cmd
@@ -1,8 +1,10 @@
 @echo off
-xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2023\"
-xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2024\"
-xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2025\"
+
+setx MAYA_ENV_DIR "%userprofile%"\Documents\maya\VFSTools\
+
+xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\VFSTools\"
 xcopy /s/y "%cd%\Maya.env" "%userprofile%\Documents\maya\2026\"
+xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\VFSTools\"
 
 xcopy /s/y "%cd%\layer_editor_tools" "%userprofile%\Documents\maya\VFSTools\layer_editor_tools\"
 xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\VFSTools\baking_tools\"
@@ -11,8 +13,5 @@ xcopy /s/y "%cd%\animation_tools" "%userprofile%\Documents\maya\VFSTools\animati
 xcopy /s/y "%cd%\leveldesign_tools" "%userprofile%\Documents\maya\VFSTools\leveldesign_tools\"
 xcopy /s/y "%cd%\icons" "%userprofile%\Documents\maya\VFSTools\icons\"
 
-xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
-xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2024\prefs\shelves\"
-xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2025\prefs\shelves\"
-xcopy /s/y "%cd%\shelf_VFS_Tools.mel" "%userprofile%\Documents\maya\2026\prefs\shelves\"
+
 
